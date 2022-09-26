a9ae1997f5b80dd4c3f73da7c2c1fa1c6baa4350
jenniferhsu0120 <jennifer.hsu.0120@gmail.com>
2022-09-25 20:33:30 -0500

    fix can't find nav bar issue

--- app/src/main/java/com/example/ambientproject/MainActivity.kt
+++ app/src/main/java/com/example/ambientproject/MainActivity.kt
@@ -22,20 +22,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         val navView: BottomNavigationView = binding.navView
         Log.i("XXX", "navView: $navView")
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//
-//        Log.i("XXX", "navController: $navController")
-//
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_mixes, R.id.navigation_sounds, R.id.navigation_settings
-//            )
-//        )
 
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
-//        // Navigation sets the title to "Simple"
-//        supportActionBar?.title = "Test"
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        Log.i("XXX", "navController: $navController")
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_mixes, R.id.navigation_sounds, R.id.navigation_settings
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        // Navigation sets the title to "Simple"
+        supportActionBar?.title = "Test"
 
     }
 }
