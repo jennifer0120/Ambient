da89181b7de76f0be5710aaf20f125f258ec36c0
jenniferhsu0120 <jennifer.hsu.0120@gmail.com>
2022-09-26 17:48:26 -0500

    set up card view for SoundsFragment

--- app/src/main/java/com/example/ambientproject/SoundsAdapter.kt
+++ app/src/main/java/com/example/ambientproject/SoundsAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.ambientproject
 
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
