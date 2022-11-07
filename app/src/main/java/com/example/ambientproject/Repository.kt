f039cf6055440696c5a060a7dd0967afaf2ca15d
jenniferhsu0120 <jennifer.hsu.0120@gmail.com>
2022-11-06 18:26:41 -0600

    add more categories

--- app/src/main/java/com/example/ambientproject/Repository.kt
+++ app/src/main/java/com/example/ambientproject/Repository.kt
@@ -6,6 +6,7 @@ class Repository {
     companion object {
         private var initialDataList = listOf (
             Data("simple_rain", "Rain", R.drawable.ic_baseline_water_drop_24, R.raw.base_after_base, false),
+            Data("rain_on_lake", "Rain Falling on Lake", R.drawable.ic_baseline_water_drop_24, R.raw.base_after_base, false),
             Data("simple_wind", "Wind", R.drawable.ic_baseline_wind_power_24, R.raw.base_after_base ,false),
             Data("simple_fire", "Fire", R.drawable.ic_baseline_fireplace_24, R.raw.base_after_base , false),
             Data("simple_bird", "Nature", R.drawable.ic_baseline_emoji_nature_24, R.raw.base_after_base , false),
