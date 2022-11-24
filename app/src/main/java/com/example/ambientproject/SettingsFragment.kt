4adbe4c4a3ca6c4559570f45e292c81549a3665b
jenniferhsu0120 <jennifer.hsu.0120@gmail.com>
2022-11-24 14:01:25 -0600

    basic database connection working

--- app/src/main/java/com/example/ambientproject/SettingsFragment.kt
+++ app/src/main/java/com/example/ambientproject/SettingsFragment.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.ambientproject.databinding.RecyclerMainBinding
 import com.example.ambientproject.databinding.SettingsRecyclerMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SettingsFragment: Fragment() {
     companion object {
@@ -18,6 +20,7 @@ class SettingsFragment: Fragment() {
 
     private var _binding: SettingsRecyclerMainBinding? = null
     private val binding get() = _binding!!
+    private val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +36,43 @@ class SettingsFragment: Fragment() {
         binding.logoutButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
         }
+
+        // Create a new user with a first and last name
+        val usertest = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+//        db.collection("users")
+//            .add(usertest)
+//            .addOnSuccessListener { documentReference ->
+//                Log.i("XXX", "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.i("XXX", "Error adding document", e)
+//            }
+//            .addOnCompleteListener { test ->
+//                Log.i("XXX", "completeListener: $test")
+//            }
+//        Log.i("XXX", "reaches here?!")
+        Log.i("XXX", "reaches here?")
+        val docRef = db.collection("users").document("9uIwD3d0H8N1JHmTqir3")
+        Log.i("XXX", "docRef: $docRef")
+        docRef
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.i("XXX", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.i("XXX", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.i("XXX", "get failed with ", exception)
+            }
+
 
         return root
     }
